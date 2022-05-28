deeeed10ad9f780f9ace0ce326de3d1843f12ac8
LovroG05 <lovro.govekar@gmail.com>
2022-05-28 22:23:35 +0200

    bugfix commit

--- studenc/backend/models.py
+++ studenc/backend/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Company(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=999, unique=True)
     
     def __str__(self):
         return self.name
 
 class Job(models.Model):
-    title = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=999)
+    location = models.CharField(max_length=999)
+    description = models.TextField(max_length=999)
     spots = models.IntegerField()
     code = models.IntegerField(unique=True)
-    neto = models.FloatField()
-    bruto = models.FloatField()
+    neto = models.FloatField(max_length=999)
+    bruto = models.FloatField(max_length=999)
     phone = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
